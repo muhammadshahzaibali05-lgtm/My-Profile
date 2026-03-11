421183ce90602e56e8434d9414d894497e3ba962
muhammadshahzaibali05-lgtm <muhammadshahzaibali05@gmail.com>
2026-03-11 15:44:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+// let name = "Ali";
+// name = "shahzaib";
+
+// console.log(name);
+
+// // Datatypes
+
+
+// 1..... primitive Data types = immitable
+
+// 1 ;string = plan text      "    ,   "
+
+// let nam = "shahzaib ";
+// console.log(name);
+
+// console.log(typeof (name));
+
+// 2; Number = whole number, integers, decimal etc
+// let age = 25
+
+
+
+// document.write("Hello")
+
+
+// console.log("M Shahzaib")
+
+// value1 = 10
+// // value 1 =12;  no space = wrong mathod
+// // 1svalue =  12;  not start with number =  wrong mathod
+// value_1 = 10;  // use underscode
+
+// //  value script is case sensitive languagr
+
+// value = 1;
+// VALUE = 2;
+// vALUE = 3;
+// value = 4;
+
+// console.log(value);
+
+// userName = "camelCase"; // use camel case
+
+// userName = "Ali";
+// father_Naame = "Shahzaib";
+// num1 = 12;
+// num2 = "12";
+
+// num3 = 12;
+// num4 = "12";
+
+// console.log(num1 + num2); // concatenation
+
+// console.log((num1 + 3) + "       " + (num2 + num4));
+
+
+// // x =10;
+// // y = 20;
+
+// // z =x + y;
+
+
+// // console.log("x=" + x);
+// // console.log("y=" + y);
+
+// // console.log0("x=y=" + z);
+
+// Task Array Assignment
+
+//1. Creat an array called numbers with CSSFontFeatureValuesRule; 1.2.3 ?
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(numbers);
+
+// 2. Creat another array called copyNumbers and copy the values of numbers into it ?
+
+// let copynumbers = [...numbers];
+// console.log(copynumbers);
+
+// Change the first value of copy Numbers to 10 ?
+// copynumbers[0] = 10;
+
+
+
+
+
